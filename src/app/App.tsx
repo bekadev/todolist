d595fd2bfb16ed78d6045f7c3667008aa525a820
beka.dev <bigsamsa3@mail.ru>
2023-02-08 21:56:38 +0600

    commit

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,7 +4,6 @@ import { TodolistsList } from '../features/TodolistsList/TodolistsList'
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
 import { Menu } from '@mui/icons-material';
